145afc2f0426af0603a608b062c3333002a9a64d
Andrea055 <vivanco11.ac@gmail.com>
2021-07-18 23:12:01 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,61 @@
+import discord
+import cpuinfo
+import psutil as p
+import os
+from discord.ext import commands
+from discord.ext.commands import bot
+
+
+def size(byte):
+  #this the function to convert bytes into more suitable reading format.
+
+  #Suffixes for the size
+  for x in ["B","KB","MB","GB","TB"]:
+    if byte<1024:
+      return f"{byte:.2f}{x}"
+    byte=byte/1024
+
+
+fre=p.cpu_freq()
+client = commands.Bot(command_prefix='.')
+info = cpuinfo.get_cpu_info()['brand_raw']
+core = 'I have ' , p.cpu_count(logical=False), 'core'
+maxfrequency = "Maximum Frequency:" ,fre.max, "Mhz"
+minfreq = "Minimum Frequency:", fre.min,"Mhz"
+currentfreq = "Current Frequency: ",fre.current ,"Mhz"
+cpusage = "Total CPU Usage:", p.cpu_percent()
+mem = p.virtual_memory()
+allram = "Total Memory:    ",size(mem.total)
+availableram = "Available Memory:", size(mem.available)
+usedram = "Used Memory:     ", size(mem.used), "percentual", mem.percent,"%"
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+
+
+@client.command()
+async def cpu(ctx):
+    await ctx.send('I have ' + info)
+
+@client.command()
+async def cpustats(ctx):
+    await ctx.send(core)
+    await ctx.send(maxfrequency)
+    await ctx.send(minfreq)
+    await ctx.send(currentfreq)
+    await ctx.send(cpusage)
+
+@client.command()
+async def ram(ctx):
+    await ctx.send(allram)
+    await ctx.send(availableram)
+    await ctx.send(usedram)
+
+@client.command()
+async def disk(ctx):
+    await ctx.send(allram)
+    await ctx.send(availableram)
+    await ctx.send(usedram)
+
+client.run('TOKEN')
